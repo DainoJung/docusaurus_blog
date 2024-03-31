50580d68cc4ad691b4cbcc70ab3633d980d73dea
DainJung <wjdekdls3693@gmail.com>
2024-03-31 19:42:55 +0900

    update soical card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -74,7 +74,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/daino-social-card.jpg',
     navbar: {
       title: "Daino's Docs",
       logo: {
